a84e50199af3f8a74b7d2f91dcea98f3199cb942
iamnern123456 <nern@onebitmatter.com>
2019-12-20 13:55:22 +0800

    Test Bot #5

--- bot.php
+++ bot.php
@@ -5,6 +5,8 @@ $access_token = '5S9dpG+aKWbMwHFN+eEy/W6HCGtWgg3iGudC7WYIEOPKdBkmz3nT3QWWVPrrDFk
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
+
+print_r($events);
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
   // Loop through each event
@@ -13,6 +15,8 @@ if (!is_null($events['events'])) {
       if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
           // Get text sent
           $text = $event['message']['text'];
+          // Get user //
+          $userid = $event['source']['userId'];
           // Get replyToken
           $replyToken = $event['replyToken'];
           // Build message to reply back
@@ -24,7 +28,7 @@ if (!is_null($events['events'])) {
           $url = 'https://api.line.me/v2/bot/message/reply';
           $data = [
               'replyToken' => $replyToken,
-              'messages' => [$messages]
+              'messages' => [$messages.$userid]
           ];
           $post = json_encode($data);
           $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
